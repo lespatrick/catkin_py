ba47f0078bbfbedf47173dc52664c91b7dacedf7
Leszek Barszcz <lpbarszcz@gmail.com>
2017-12-29 15:33:56 +0100

    build files removed

--- catkin_robo_ws/build/motor_hat_receiver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leszek/catkin_py/catkin_robo_ws/src/motor_hat_receiver
-# Build directory: /home/leszek/catkin_py/catkin_robo_ws/build/motor_hat_receiver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_robo_ws/build/robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leszek/catkin_py/catkin_robo_ws/src/robot_description
-# Build directory: /home/leszek/catkin_py/catkin_robo_ws/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
